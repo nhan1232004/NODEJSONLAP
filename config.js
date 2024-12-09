fb36ab2a6f328fcedb8bb91d0c6ee5f330aedaea
nhan1232004 <nguyenhuunhan2313@gmail.com>
2024-12-09 14:04:12 +0700

    init project

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  app: {
-    port: 3000
-  },
-  database: {
-    connection: 'mongodb://localhost:27017/shopping',
-    option: {
-      useMongoClient: true,
-      autoIndex: false
+    app: {
+        port: process.env.PORT || 3000 // Sử dụng PORT do Railway cung cấp hoặc mặc định là 3000
+    },
+    database: {
+        connection: process.env.MONGO_URI || 'mongodb://localhost:27017/shopping',
+        option: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            autoIndex: false
+        }
+    },
+    session: {
+        key: process.env.SESSION_KEY || '27bda112-99dd-4496-8015-ea20d1034228' // Sử dụng SESSION_KEY từ biến môi trường
     }
-  },
-  session: {
-    key: '27bda112-99dd-4496-8015-ea20d1034228'
-  }
 };
